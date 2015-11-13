b1df4369c0f46a3ad1c899030850e56307da8b88
rubencc <rubencc@gmail.com>
2015-11-13 14:50:14 +0100

    Clase de test

---
+++ Test/Business/PrimeFactorsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Business
+{
+    [TestClass]
+    public class PrimeFactorsTest
+    {
+        private PrimeFactors primeFactors;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.primeFactors = new PrimeFactors();
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            this.primeFactors = null;
+        }
+
+
+    }
+}
