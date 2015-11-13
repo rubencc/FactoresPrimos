709780f413c7d1b13c0f0026e5133022e4c97415
rubencc <rubencc@gmail.com>
2015-11-13 14:46:25 +0100

    Proyecto base

---
+++ Business/PrimeFactors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class PrimeFactors : IPrimeFactors
+    {
+        public List<int> Calculate(int input)
+        {
+            return new List<int>();
+        }
+    }
+}
